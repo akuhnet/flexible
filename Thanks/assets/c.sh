6dd1c2842941ae65bedf37137f0bf4dba0ec22dd
akuh <81416541+akuhnet@users.noreply.github.com>
2021-10-06 14:42:53 +0700

    Update c.sh

--- Thanks/assets/c.sh
+++ Thanks/assets/c.sh
@@ -1,8 +1,1 @@
 #!/bin/bash
-clear
-
-vtx=$(egrep -i '^flags.*(vmx|svm)' /proc/cpuinfo | wc -l)
-if [ $vtx = 0 ] ; then echo "[Error] Environment Checking Error!!! Please Reload Page!!!"&& shutdown now
-fi
-
-clear
