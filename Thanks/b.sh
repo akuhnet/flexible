1bccfe68738b95783502346f4a19e7500fe89780
akuh <81416541+akuhnet@users.noreply.github.com>
2021-10-06 14:50:51 +0700

    Update b.sh

--- Thanks/b.sh
+++ Thanks/b.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-
-
-if [ "$HOSTNAME" = controlplane ]; then
-    while [ ! -f /usr/local/bin/wait.sh ]; do sleep 1; done; scp -r /usr/local/bin/wait.sh node01:/usr/local/bin/
-    ssh node01 
-
-else
-    sleep 999999
-fi
-  
 source c.sh
